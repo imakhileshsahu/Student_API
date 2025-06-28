1af376984967b0b859d891aa1102329bbfcedbc7
Akhilesh sahu <akhileshsahu@Akhileshs-Mac.local>
2025-06-28 15:04:01 +0530

    Initial commit

---
+++ internal/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string `yaml:"address" env-required:"true"`
+}
+
+//`yaml:"env" env:"ENV" env-required:"true"` this is struct tag
+
+// env-default:"production"
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+
+	// first try environment variable
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+	// error handling more important in go
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("can not read config file: %s", err.Error())
+	}
+
+	return &cfg
+}
